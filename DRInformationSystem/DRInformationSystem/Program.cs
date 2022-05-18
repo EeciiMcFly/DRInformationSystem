025d6afd2c00520c5d6561ad0e35434622794322
Shahzod <48061105+EeciiMcFly@users.noreply.github.com>
2022-05-19 01:09:31 +0500

    Remove trash files

--- DRInformationSystem/DRInformationSystem/Program.cs
+++ DRInformationSystem/DRInformationSystem/Program.cs
@@ -16,13 +16,8 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
-
-
 app.MapControllerRoute(
 	name: "default",
 	pattern: "{controller}/{action=Index}/{id?}");
-
 app.MapFallbackToFile("index.html");
-;
-
 app.Run();
